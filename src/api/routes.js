6f0a6665ec849b8f4ed460e53e7ca0b2a3af6755
Josephns85179 <josephns85179@gmail.com>
2026-04-22 20:12:56 +0700

    tambah kehadiran

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
-const users = require("./components/users/users-route");
-const auth = require("./components/auth/auth-route");
+const users = require('./components/users/users-route');
+const auth = require('./components/auth/auth-route');
 
 module.exports = () => {
   const app = express.Router();
 
   users(app);
-
+  auth(app);
   return app;
 };
