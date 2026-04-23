b355af1dd0070817c7a6cb598439f19206b106f1
rizkiard1-kyy <rizkiardi405@gmail.com>
2026-04-23 20:49:00 +0700

    add api biodata

--- src/api/routes.js
+++ src/api/routes.js
@@ -5,6 +5,7 @@ const auth = require('./components/auth/auth-route');
 const rps = require('./components/RPS/RPS-route');
 const skpi = require('./components/skpi/skpi-route');
 const jadwalKuliah = require('./components/akademic/JadwalKuliah/JadwalKuliah-route');
+const biodata = require('./components/biodata/biodata/biodata-route');
 
 module.exports = () => {
   const app = express.Router();
@@ -14,5 +15,6 @@ module.exports = () => {
   rps(app);
   skpi(app);
   jadwalKuliah(app);
+  biodata(app);
   return app;
 };
