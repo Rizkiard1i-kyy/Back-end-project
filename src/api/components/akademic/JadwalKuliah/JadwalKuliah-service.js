b95a3e1027c14a8a98821f5a71025265acd95991
Chr155555 <christian.rz1678@gmail.com>
2026-04-23 14:24:19 +0700

    final, udah di masukin fitur lock

--- src/api/components/akademic/JadwalKuliah/JadwalKuliah-service.js
+++ src/api/components/akademic/JadwalKuliah/JadwalKuliah-service.js
@@ -1,25 +1,26 @@
 const jadwalRepo = require('./JadwalKuliah-repository');
 
 async function ambilJadwal(filterSemester) {
-    let queryDatabase = {};
-    
-    if (filterSemester) {
-        // Otomatis merapikan inputan dari URL: "genap" -> "Genap"
-        const semesterRapi = filterSemester.charAt(0).toUpperCase() + filterSemester.slice(1).toLowerCase();
-        queryDatabase.semester = semesterRapi; 
-    }
+  let queryDatabase = {};
 
-    return await jadwalRepo.cariJadwal(queryDatabase);
+  if (filterSemester) {
+    const semesterRapi =
+      filterSemester.charAt(0).toUpperCase() +
+      filterSemester.slice(1).toLowerCase();
+    queryDatabase.semester = semesterRapi;
+  }
+
+  return await jadwalRepo.cariJadwal(queryDatabase);
 }
 
 async function buatJadwalBaru(dataDariController) {
-    if (!dataDariController.mataKuliah) {
-        throw new Error("Nama mata kuliah wajib diisi!");
-    }
-    return await jadwalRepo.tambahJadwal(dataDariController);
+  if (!dataDariController.mataKuliah) {
+    throw new Error('Nama mata kuliah wajib diisi!');
+  }
+  return await jadwalRepo.tambahJadwal(dataDariController);
 }
 
 module.exports = {
-    ambilJadwal,
-    buatJadwalBaru,
+  ambilJadwal,
+  buatJadwalBaru,
 };
